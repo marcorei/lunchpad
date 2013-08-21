23b32789b571b9b1ad059161b1e39e5f1c5bb4ad
Markus Riegel <mr@19h13.com>
2013-08-21 22:05:49 +0200

    mysql query escaping and more routes

--- node/app.js
+++ node/app.js
@@ -33,32 +33,23 @@ var	connection = mysql.createConnection({
 });
 
 
+
+function esc( str ){
+	return connection.escape( str );
+}
+
+function escId( id ){
+	return mysql.escapeId( id ;
+}
+
+
 // callback( err,user )
+function getUserBySid( sid, callback ){
 
-function getUser( username, callback ){
-
-	connection.query("SELECT * FROM  `users` WHERE  `email` = '" + username + "' LIMIT 0 , 1",
+	connection.query("SELECT * FROM  `users` WHERE  `sid` = '" + esc(sid) + "' LIMIT 0 , 1",
 		function( err, rows ) {
 
-			//console.log('getting user: ' + username);
-
-			var user = {};
-
-			if( !err ){
-
-				//console.log('found user: ' + username);
-
-				user.id = rows[0].id;
-				user.username = rows[0].email;
-				user.password = rows[0].password;
-				user.notify = rows[0].notify;
-				user.image = rows[0].image;
-
-			}
-
-			//console.log('calling callback: ' + callback);
-
-			callback( err, user );
+			composeUserObj( err, rows, callback );
 
 		}
 	);
@@ -66,10 +57,49 @@ function getUser( username, callback ){
 }
 
 
+// callback( err,user )
+function getUserByName( username, callback ){
+
+	connection.query("SELECT * FROM  `users` WHERE  `email` = '" + esc(username) + "' LIMIT 0 , 1",
+		function( err, rows ) {
+
+			composeUserObj( err, rows, callback );
+
+		}
+	);
+
+}
+
+
+function composeUserObj( err, rows, callback )
+{
+	//console.log('getting user: ' + username);
+
+	var user = {};
+
+	if( !err ){
+
+		//console.log('found user: ' + username);
+
+		user.id = rows[0].id;
+		user.sid = rows[0].sid;
+		user.username = rows[0].email;
+		user.password = rows[0].password;
+		user.notify = rows[0].notify;
+		user.image = rows[0].image;
+
+	}
+
+	//console.log('calling callback: ' + callback);
+
+	callback( err, user );
+}
+
+
 // callback( err, count )
 function checkUserExists( username, callback ){
 
-	connection.query("SELECT count(*) AS `numusers` FROM  `users` WHERE  `email` = '" + username + "'",
+	connection.query("SELECT count(*) AS `numusers` FROM  `users` WHERE  `email` = '" + esc(username) + "'",
 		function( err, rows ) {
 
 			callback( err, rows[0].numusers );
@@ -85,12 +115,14 @@ function createUser( username, passwort, image ){
 	console.log("host: " + config.mysql.host);
 
 	// Insert User into db
-	connection.query("INSERT INTO `project130820db`.`users` (`id`, `email`, `password`, `notify`, `image`) VALUES (NULL, '" + 
-		username + 
+	connection.query("INSERT INTO `project130820db`.`users` (`id`, `sid`, `email`, `password`, `notify`, `image`) VALUES (NULL, '" + 
+		passwordHash.generate( username ) + 
+		"', '" + 
+		esc(username) + 
 		"', '" + 
 		passwordHash.generate( passwort ) +
 		"', '0', '" +
-		image +
+		esc(image) +
 		"')" ,
 
 		function( err ) {
@@ -107,6 +139,34 @@ function createUser( username, passwort, image ){
 }
 
 
+// callback(err)
+function changeUserNotify( username, notify, callback ){
+
+	// Authentification is done.
+	// So check if there is something to change and change it.
+
+}
+
+
+
+// callback(err,venue)
+// @param venue object {name,url}
+function createVenue( venue, callback ){
+
+	// check if venue with that nam exists.
+
+	// query etc
+
+}
+
+
+// callback(err,venues)
+function getVenueList( callback ){
+
+	// recursive checks that list all venues, corresponding users with images.
+
+}
+
 
 
 
@@ -119,7 +179,16 @@ function createUser( username, passwort, image ){
  * App
  */
 
- var app = express();
+var app = express();
+
+
+// app setup
+
+app.use( express.cookieParser() );
+app.use( express.bodyParser() );
+app.use( express.session({ secret: 'Juergen', maxAge: 360*5 }) );
+app.use( passport.initialize() );
+app.use( passport.session() );
 
 
 // Static content
@@ -128,30 +197,29 @@ app.use('/static', express.static(__dirname + '/../static'));
 
 
 
+
+
+
+
+
+
+
+
+
 // passport fixes
 
 passport.serializeUser(function(user, done) {
-	done(null, user.username);
+	done(null, user.sid);
 });
 
-passport.deserializeUser(function(username, done) {
-	getUser( username, function (err, user) {
+passport.deserializeUser(function(sid, done) {
+	getUserBySid( sid, function (err, user) {
 		done(err, user);
 	}); 
-
-	//User.findOne(id, function (err, user) {
-	//	done(err, user);
-	//});
 });
 
 
 // passport setup
-
-app.use( express.cookieParser() );
-app.use( express.bodyParser() );
-app.use( express.session({ secret: 'Juergen', maxAge: 360*5 }) );
-app.use( passport.initialize() );
-app.use( passport.session() );
 
 passport.use( new LocalStrategy(
 
@@ -160,7 +228,7 @@ passport.use( new LocalStrategy(
 		console.log('starting auth strategie for user: ' + username);
 
 		// query for user in db
-		getUser( username, function( err, user ){
+		getUserByName( username, function( err, user ){
 
 			if( err ){ 
 				console.log( err );
@@ -184,10 +252,6 @@ passport.use( new LocalStrategy(
 
 ));
 
-
-
-
-
 // passport helper
 
 function requireAuth( req, res, next ){
@@ -200,23 +264,34 @@ function requireAuth( req, res, next ){
 
 }
 
-/* ONLY FOR REGISTRATION IT SEEMS
-function authUserExist( req, res, next ) {
+function requireAuthJSON( req, res, next ){
 
-	
+	if( req.isAuthenticated() ){
+        next();
+    }else{
 
-	checkUserExists( req.body.username, function( err, count){
-
-		if( count == 0) {
-			next();
-		}else{
-			res.redirect('/signup');
-		}
-
-	});
+    	var error = { error: "authentification required" }
+        res.json(error);
+    }
 
 }
-*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ * Routes
+ *
+ */
+
 
 
 
@@ -272,6 +347,10 @@ app.get( '/logout', function(req, res){
 
 
 
+
+
+
+
 // Root
 
 app.get( '/', function(req, res){
@@ -301,17 +380,111 @@ app.post( '/', function(req, res){
 });
 
 
-/*
-app.get( '/', 
-	passport.authenticate('local'), 
-	function(req, res){
 
-		var file = '../templates/page.app.html';
-		res.sendfile( path.join( __dirname, file ) );
+
+
+
+
+
+
+
+
+
+
+// Put, Get Change stuff
+
+
+// get venues
+
+app.get( '/get/venues', function(req, res){
+
+	requireAuthJSON( req, res, function(){
+		
+		getVenueList( function(err, venues){
+
+			if(err){
+				console.log(err);
+				res.json({ error: "venue list could not be created."});
+			}else{
+				res.json({
+					error: "",
+					venues: venues
+				});
+			}
+		});
 
 	});
 
-*/
+});
+
+
+// post venue
+// data: venuename, venueurl
+
+app.post( '/post/venue', function(req, res){
+
+	requireAuthJSON( req, res, function(){
+
+		var v = {
+			name: req.body.venuename,
+			url: req.body.venueurl
+		}
+		createVenue( v, function(err,venue){
+
+			if( err ){
+				console.log(err);
+				res.json({ error: "venue could not be created."});
+			}else{
+				res.json({
+					error: "",
+					venuename: venue.name
+				});
+			}
+
+		});
+
+	});
+
+});
+
+
+// change notification
+
+app.post( '/post/notify', function(req, res){
+
+	requireAuthJSON( req, res, function(){
+
+		//req.body.notify
+		//req.user.username
+
+	});
+
+});
+
+
+// add / change visit
+
+app.post( '/post/visit', function(req, res){
+
+	requireAuthJSON( req, res, function(){
+
+		//req.body.venueid
+
+		//req.user.username
+
+		//check if existing connection (on that date): change, or create.
+		//function callback with (err)
+
+
+		// SEND NOTIFICATIONS
+
+	});
+
+})
+
+
+// 
+
 
 
 
