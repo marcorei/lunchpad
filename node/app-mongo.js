26a2d00203b906848bdce670fb2a34523222c0d6
Markus Riegel <mr@19h13.com>
2014-02-26 09:58:47 +0100

    basic app module config structure

--- node/app-mongo.js
+++ node/app-mongo.js
@@ -12,15 +12,29 @@
 
 var path = require('path'),
 	express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+
+	io = require('socket.io')(server),
+	MongoStore = require('connect-mongo')(express),
+	passportSocketIo = require('passport.socketio'),
+
 	passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
 	passwordHash = require('password-hash'),
+
 	Validator = require('validator').Validator,
+
 	nodemailer = require('nodemailer'),
-	VenueProvider = require('./venueprovider-memory').VenueProvider,
-	UserProvider = require('./userprovider-memory').UserProvider,
-	ItemProvider = require('./itemprovider-memory').UserItemProvider,
+
+	VenueProvider = require('./venueprovider-mongo.js').VenueProvider,
+	UserProvider = require('./userprovider-mongo.js').UserProvider,
+	ItemProvider = require('./itemprovider-mongo.js').ItemProvider,
+
 	config = require('./config.json');
+
+
+
 
 
 
@@ -52,6 +66,50 @@ Date.prototype.yyyymmdd = function() {
  * Helper functions
  */
 
+var lunchHelper = {
+	sendFile = function(fileName){
+		return function(req, res){
+			res.sendFile( path.join( __dirname, fileName ));
+		}
+	}
+}
+
+
+
+
+
+
+/*
+ * Session store setup
+ */
+
+var mongoStore = new MongoStore({
+	host: config.mongo.host,
+	port: config.mongo.port,
+	db: config.mongo.db
+	// stringify: true
+});
+
+
+
+
+
+
+/*
+ * io config
+ */
+
+io.set('authorization', passportSocketIo.authorize({
+	cookieParser: express.cookieParser,
+	key:         config.cookie.key,
+	secret:      config.secret,
+	store:       mongoStore
+	//success:     onAuthorizeSuccess,
+	//fail:        onAuthorizeFail
+}));
+
+//socket.handshake.user
+// This property is always available from inside a io.on('connection') handler.
 
 
 
@@ -60,17 +118,19 @@ Date.prototype.yyyymmdd = function() {
 
 
 /*
- * App basics
+ * Express setup
  */
-
-var app = express();
 
 
 // app setup
 
 app.use( express.cookieParser() );
 app.use( express.bodyParser() );
-app.use( express.session({ secret: 'Juergen', maxAge: 360*5 }) );
+app.use( express.session({ 
+	store: MongoStore,
+	secret: config.secret,
+	key: config.cookie.key
+}));
 app.use( passport.initialize() );
 app.use( passport.session() );
 
@@ -88,32 +148,72 @@ app.use('/static', express.static(__dirname + '/../static'));
 
 
 /*
- * Passport stuff
+ * Passport setup
  */
+
+
+// Serialization
+
+passport.serializeUser(function(user, done) {
+	done(null, user._id);
+});
+
+passport.deserializeUser(function(_id, done) {
+	//( _id, function (err, user) {
+		//done(err, user);
+	//}); 
+
+	// Find Mongo User
+});
+
 
 
 // Strategies etc
 
+passport.use('local-login', new LocalStrategy(
+	function( username, password, done ){
+		console.log('starting auth strategie for user: ' + username);
 
-// Auth Middleware
+		// query for user in db
+		// needs to be ajusted to userprovider
 
+		/*
+		getUserByName( username, function( err, user ){
+
+			if( err ){ 
+				console.log( err );
+				return done( err ); 
+			}
+			if( !user ){ 
+				console.log('user not found: ' + username);
+				return done( null, false, {message:'Incorrect username.'} ); 
+			}
+			if( !passwordHash.verify( password, user.password ) ){ 
+				console.log('password did not match for user: ' + username);
+				return done( null, false, {message:'Incorrect password.'} ); 
+			}
+
+			console.log('user authenticated: ' + user.username);
+			return done( null, user );
+
+		});
+*/
+	}
+));
+
+
+
+// Auth Helper
+// probably do this inside a provider, zb userProvider.isAdmin(uid, callback)
+/*
 var lunchAuth = {
 
 	requireUser = function(){
-		return passport.authenticate('local');
-	}
+		
+	},
 
-	requireGroup = function(group){
-		return [
-			passport.authenticate('local'),
-			function(req, res, next) {
-				if (req.user && req.user.group === group){
-					next();
-				}else{
-					res.send(401, 'Unauthorized');
-				}
-			}
-		]
+	requireGroup = function(groupId){
+		
 	},
 
 	requireOwnerOrAdmin = function(owner_id){
@@ -121,6 +221,13 @@ var lunchAuth = {
 	}
 
 };
+*/
+
+
+
+
+
+
 
 
 
@@ -130,8 +237,92 @@ var lunchAuth = {
 
 
 /*
- * Routes
+ * Express Routes
  */
+
+
+// Define details to change in one place
+
+var lunchPages = {
+	root: { 
+		route: '/',
+		tmpl: 'page.app.angular.html'
+	}
+	login: {
+		route: '/login',
+		tmpl: 'page.login.html'
+	},
+	manifesto: {
+		route: '/manifesto',
+		tmpl: 'page.manifesto.html'
+	},
+
+
+
+	viewVenuelist: {
+		route: '/view/venuelist',
+		tmpl: 'view.venuelist.html'
+	},
+	viewVenueDetail: {
+		route: '/view/venuedetail',
+		tmpl: 'view.venuedetail.html'
+	},
+	viewNewVenue: {
+		route: '/view/newvenue',
+		tmpl: 'view.newvenue.html'
+	},
+	viewSettings: {
+		route: '/view/settings',
+		tmpl: 'view.settings.html'
+	}
+}
+
+
+var lunchActions = {
+	login: '/action/login',
+	logout: '/action/logout'
+}
+
+
+
+// Pages. Just get, no data.
+
+app.get( lunchPages.root.route, 		lunchHelper.sendFile( lunchPages.root.tmpl ) );
+app.get( lunchPages.login.route,		lunchHelper.sendFile( lunchPages.login.tmpl ) );
+app.get( lunchPages.manifesto.route,	lunchHelper.sendFile( lunchPages.manifesto.tmpl ) );
+
+app.get( lunchPages.viewVenuelist.route, 	lunchHelper.sendFile( lunchPages.viewVenuelist.tmpl ) );
+app.get( lunchPages.viewVenueDetail.route, 	lunchHelper.sendFile( lunchPages.viewVenueDetail.tmpl ) );
+app.get( lunchPages.viewNewVenue.route,		lunchHelper.sendFile( lunchPages.viewNewVenue.tmpl ) );
+app.get( lunchPages.viewSettings.route,		lunchHelper.sendFile( lunchPages.viewSettings.tmpl ) );
+
+
+
+// Action-Requests not send via socket (login / logout via passport)
+
+app.post( lunchActions.login, passport.authenticate('local-login'), function(req,res){
+	if(req.isAuthenticated())
+		res.json({ error: null });
+	else
+		res.json({ error:{msg:'login failed'} });
+});
+
+app.post( lunchActions.logout, function(req,res){
+	req.logout();
+	res.json({ error: null });
+});
+
+
+
+
+
+
+
+
+/*
+ * Socket.io "Routes"
+ */
+
 
 
 
