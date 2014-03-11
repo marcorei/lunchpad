edad8a35b8ec83a63ef7a796bae653d1e86a7053
Markus Riegel <mr@19h13.com>
2014-03-11 22:10:45 +0100

    quando module

---
+++ node/module.quando.js
@@ -0,0 +1,47 @@
+/*
+ * Lunchpad Time module
+ *
+ * @author Markus Riegel <riegel.markius@googlemail.com>
+ */
+
+
+var Quando = function(){};
+
+Quando.prototype.today = function(){
+	var now = new Date();
+	return new Date(now.getYear(),now.getMonth(),now.getDate());
+}
+
+Quando.prototype.min15 = function(){
+	var now = new Date(),
+	now.setMinutes(now.getMinutes()-15);
+	return now;
+}
+
+Quando.prototype.weekday = function(){
+	return new Date().getDay();
+}
+
+Quando.prototype.daysAgo = function(numDays){
+	var day = Quando.prototype.today();
+	day.setDate(today.getDate()-numDays);
+	return day;
+}
+
+Quando.prototype.l7d = function(){
+	Quando.prototype.daysAgo(7);
+};
+
+Quando.prototype.l30d = function(){
+	Quando.prototype.daysAgo(30);
+};
+
+Quando.prototype.l35d = function(){
+	Quando.prototype.daysAgo(35);
+};
+
+Quando.prototype.l60d = function(){
+	Quando.prototype.daysAgo(60);
+};
+
+exports.quando = new Quando();
