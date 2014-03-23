7d9de3506d4bfa3a049ba59a571d92f9c7a84c23
Markus Riegel <riegel.markus@googlemail.com>
2014-03-23 22:38:40 +0100

    indent

--- dev/js/module.socket.js
+++ dev/js/module.socket.js
@@ -7,68 +7,63 @@
  */
 
 angular.factory('Socket',[
-	'$rootScope',
-	function($rootScope){
+'$rootScope',
+function($rootScope){
 
-		var socket = io.connect();
+	var socket = io.connect();
 
-		var on = function(event, callback){
-			socket.on(event, function(){
-				var args = arguments;
-				$rootScope.$apply(function(){
-					callback.apply(this, args);
-				});
+	var on = function(event, callback){
+		socket.on(event, function(){
+			var args = arguments;
+			$rootScope.$apply(function(){
+				callback.apply(this, args);
 			});
-		}
+		});
+	}
 
-		var off = function(event, callback){
-			socket.removeListener(event, callback);
-		}
+	var off = function(event, callback){
+		socket.removeListener(event, callback);
+	}
 
-		var emit = function(event, data, callback)){
-			socket.emit(event, data, function(){
-				var args = arguments;
-				$rootScope.$apply(function(){
-					callback.apply(this, args);
-				});
+	var emit = function(event, data, callback)){
+		socket.emit(event, data, function(){
+			var args = arguments;
+			$rootScope.$apply(function(){
+				callback.apply(this, args);
 			});
-		}
+		});
+	}
 
 
 
-		var SocketManager = function(scope){
-			var listeners = [];
-			this.listeners = listeners;
+	var SocketManager = function(scope){
+		var listeners = [];
+		this.listeners = listeners;
 
-			scope.$on('$destroy',function(){
-				var i;
-				for(i=0;i<listeners.length;i++){
-					off(listeners[i][0],listeners[i][1]);
-				}
+		scope.$on('$destroy',function(){
+			var i;
+			for(i=0;i<listeners.length;i++){
+				off(listeners[i][0],listeners[i][1]);
+			}
+		});
+	}
 
-				listeners = null;
-				this.listeners = null;
-			});
+	SocketManager.prototype.on = function(event, callback){
+		this.listeners.push([event,callback]);
+		on(event,callback);
+	}
 
+	SocketManager.prototype.emit = emit;
 
-		}
-
-		SocketManager.prototype.on = function(event, callback){
-			this.listeners.push([event,callback]);
-			on(event,callback);
-		}
-		
-		SocketManager.prototype.emit = emit;
-
-		var generateManager = function(){
-			return new SocketManager();
-		}
+	var generateManager = function(){
+		return new SocketManager();
+	}
 
 
-		return {
-			on: on,
-			off: off,
-			emit: emit,
-			generateManager: generateManager
-		}
+	return {
+		on: on,
+		off: off,
+		emit: emit,
+		generateManager: generateManager
+	}
 }]);
