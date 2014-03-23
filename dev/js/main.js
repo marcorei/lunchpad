7c13e8998f9545ce554c584adc7fa6112b619399
Markus Riegel <riegel.markus@googlemail.com>
2014-03-23 22:38:09 +0100

    some routes and stuff for the main angular file

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,3 +7,54 @@
  */
 
 
+
+angular.module('lunchpad',[
+	'ngAnimate',
+	'ngRoute',
+
+	'viewVenuesController',
+	'viewVenueDetailController',
+	'viewSettingsController'
+])
+
+.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/venues', {
+
+				templateUrl: './view.venuelist.html',
+				controller: 'ViewVenuesController'
+
+			}).
+			when('/venue/:venueId', {
+
+				templateUrl: './view.venuedetail.html',
+				controller: 'ViewVenuesDetailController'
+
+			}).
+			when('/settings', {
+
+				templateUrl: './view.settings.html',
+				controller: 'ViewSettingsController'
+
+			}).
+			otherwise({
+
+				redirectTo: '/venues'
+
+			});
+	}
+])
+
+.provider('$anchorScroll',function() {
+	this.$get = ['$window', '$location', '$rootScope', function($window, $location, $rootScope) {
+		function scroll() {}
+		return scroll;
+	}];
+});
+
+
+
+
+
+
