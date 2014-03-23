61bb8f33b1fa9c0430033871078168d5eacc4b37
Markus Riegel <riegel.markus@googlemail.com>
2014-03-23 19:36:28 +0100

    Socket angular module

---
+++ dev/js/module.socket.js
@@ -0,0 +1,74 @@
+/*
+ * Socket service
+ * You can either use it directly: inject, add listeners with on and remove them with off.
+ * Or: generate a manager that removes sockets automaticly as the scope gets destroyed.
+ *
+ * @author Markus Riegel <riegel.markius@googlemail.com>
+ */
+
+angular.factory('Socket',[
+	'$rootScope',
+	function($rootScope){
+
+		var socket = io.connect();
+
+		var on = function(event, callback){
+			socket.on(event, function(){
+				var args = arguments;
+				$rootScope.$apply(function(){
+					callback.apply(this, args);
+				});
+			});
+		}
+
+		var off = function(event, callback){
+			socket.removeListener(event, callback);
+		}
+
+		var emit = function(event, data, callback)){
+			socket.emit(event, data, function(){
+				var args = arguments;
+				$rootScope.$apply(function(){
+					callback.apply(this, args);
+				});
+			});
+		}
+
+
+
+		var SocketManager = function(scope){
+			var listeners = [];
+			this.listeners = listeners;
+
+			scope.$on('$destroy',function(){
+				var i;
+				for(i=0;i<listeners.length;i++){
+					off(listeners[i][0],listeners[i][1]);
+				}
+
+				listeners = null;
+				this.listeners = null;
+			});
+
+
+		}
+
+		SocketManager.prototype.on = function(event, callback){
+			this.listeners.push([event,callback]);
+			on(event,callback);
+		}
+		
+		SocketManager.prototype.emit = emit;
+
+		var generateManager = function(){
+			return new SocketManager();
+		}
+
+
+		return {
+			on: on,
+			off: off,
+			emit: emit,
+			generateManager: generateManager
+		}
+}]);
