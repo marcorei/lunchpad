4b9cb01b1ac3194955988efa27c7c417a976f3af
Markus Riegel <riegel.markus@googlemail.com>
2014-03-29 23:05:12 +0100

    added lots of logic to the venue list service

--- dev/js/service.venue.js
+++ dev/js/service.venue.js
@@ -5,14 +5,15 @@
  */
 
 
-angular.module('venueService',[
+angular.module('lpVenueService',[
 	'socket',
-	'lpConfig'
+	'lpConfig',
+	'lpError'
 ])
 
-.factory('VenueService',[
-'Socket','LpConfig'
-function(Socket){
+.factory('LpVenueService',[
+'Socket','LpConfig','LpError',
+function(Socket,LpConfig,LpError){
 
 	var venues = [],
 		queue = [],
@@ -20,49 +21,179 @@ function(Socket){
 		socketManager = Socket.generateManager();
 
 	var getVenueById = function(id, callback){
-		// check if loaded, else push to queue
-
-	}
+		if(loaded){
+			callback(venues[findVenueById(id)]);
+		}else{
+			queue.push([getVenueById,[id,callback]]);
+		}
+	};
 
 	var loadVenues = function(callback){
 		socketManager.emit(LpConfig.getEvent('venue.read.list'),{},function(data){
 			if(!data.error){
 				venues = data.venues;
+				loaded = true;
 				if(callback) callback();
 			}else{
-				console.log(data.error);
-				// Send to Error!
+				LpError.throw(LpError.getMsg(data.error.code) || data.error.msg);
 			}
 		});
+	};
+
+	var findVenueIndexById = function(id){
+		var i,
+			venue,
+			length = venues.length;
+		for(i = 0; i < venues.length; i++){
+			venue = venues[i];
+			if(venue._id === id) return i;
+		}
+		return -1;
+	};
+
+	var checkPositionForIndex = function(index){
+		var c = venues[index].guests.length,
+			d,
+			tc,
+			i,
+			span;
+		
+		span = index;
+		for(i = 0; i < span; i++){
+			d = venues[index - (1 + i)].guests.length;
+			if(c < d){
+				tc = index - (1 + i);
+			}else{
+				break;
+			}
+		}
+
+		if(tc){	return tc; }
+
+		span = venues.length - index;
+		for(i = 0; i < span; i++){
+			d = venue[index + (i + 1)].guest.length;
+			if(c > d){
+				tc = index + (i + 1);
+			}else{
+				break;
+			}
+		}
+
+		return tc || index;
 	}
+
+	var rearrangeVenue = function(index){
+		var tc = checkPositionForIndex(index);
+		if(tc != index){
+			venues.splice(tc,0,venues.splice(index,1)[0]);
+		}
+	}
+
+
+
+
+
 
 	var onUnhandeltModify = function(){
+		if(loaded){
+			loaded = false;
+			loadVenues();
+		}
+	};
 
-	}
+	var onCheckinCreateDone = function(data){
+		var index = findVenueIndexById(data.vid);
+		if(index < 0){
+			onUnhandeltModify;
+			return null;
+		}
+		venues[index].guests.push(data.user);
+		rearrangeVenue(index);
+	};
 
-	var onCheckinCreateDone = function(){
+	var onCheckinDeleteDone = function(data){
+		var index,
+			venue,
+			i,
+			j;
+		for(i = 0; i < venues.length; i++){
+			guests = venues[i].guests;
+			for(j = 0; j < guests.length; j++){
+				if(guests[j]._id === data.user._id){
+					index = i;
+					guests.splice(j,1);
+					break;
+				}
+			}
+			if(index !== 'undefined'){
+				break;
+			}
+		}
+		rearrangeVenue(index);
+	};
 
-	}
+	var onVenueUpdateNameDone = function(data){
+		var index = findVenueIndexById(data.venue._id);
+		if(index < 0){
+			onUnhandeltModify;
+			return null;
+		}
+		venues[index].name = data.venue.name;
+	index
+	var onVenueUpdateUrlDone = function(data){
+		var index = findVenueIndexById(data.venue._id);
+		if(index < 0){
+			onUnhandeltModify;
+			return null;
+		}
+		venues[index].url = data.venue.url;
+	};
 
-	var onCheckinDeleteDone = function(){
+	var onCommentCreateDone = function(data){
+		var index = findVenueIndexById(data.comment.vid);
+		if(index < 0){
+			onUnhandeltModify;
+			return null;
+		}
+		venues[index].comc = data.count;
+	};
 
-	}
+	var onCommentDeleteDone = function(data){
+		var index = findVenueIndexById(data.comment.vid);
+		if(index < 0){
+			onUnhandeltModify;
+			return null;
+		}
+		venues[index].comc = data.count;
+	};
 
 	// init:load venues
 		// subscribe to events
 
 
 	loadVenues(function(){
-		loaded = true;
-		// work the queue
-		// add events
+		var queueItem;
+		while(queue.length > 0){
+			queueItem = queue.pop();
+			queueItem[0].apply(this,queueItem[1]);
+		}
+		
+		socketManager.on(LpConfig.getEvent('checkin.create.done'),onCheckinCreateDone);
+		socketManager.on(LpConfig.getEvent('checkin.delete.done'),onCheckinDeleteDone);
+		socketManager.on(LpConfig.getEvent('venue.update.name.done'),onVenueUpdateNameDone);
+		socketManager.on(LpConfig.getEvent('venue.update.url.done'),onVenueUpdateUrlDone);
+		socketManager.on(LpConfig.getEvent('comment.create.done'),onCommentCreateDone);
+		socketManager.on(LpConfig.getEvent('comment.delete.done'),onCommentDeleteDone);
 
+		socketManager.on(LpConfig.getEvent('venue.create'),onUnhandeltModify);
+		socketManager.on(LpConfig.getEvent('venue.delete'),onUnhandeltModify);
 	});
 
 
 	return {
 		venues = venues, // bind to this
-		getVenueById = getVenueById
+		getVenueById = getVenueById // shortcut with queuing and callback
 	};
 
 }]);
