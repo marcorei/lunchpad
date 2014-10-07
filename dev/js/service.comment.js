faf921c71737cc2f259b395fe536dca3396a21d4
Markus Riegel <riegel.markus@googlemail.com>
2014-10-07 19:53:52 +0200

    stubs for comment and user service

---
+++ dev/js/service.comment.js
@@ -0,0 +1,22 @@
+/*
+ * Service to request and cache a comment thread
+ *
+ * @author Markus Riegel <riegel.markus@googlemail.com>
+ */
+
+angular.module('lpCommentService',[
+	'socket',
+	'lpConfig'
+])
+
+.factory('LpCommentService',[
+'Socket','LpConfig',
+function(Socket,LpConfig){
+
+	
+
+	return {
+
+	};
+
+}]);
---
+++ dev/js/service.user.js
@@ -0,0 +1,23 @@
+/*
+ * Service to request user data
+ *
+ * @author Markus Riegel <riegel.markus@googlemail.com>
+ */
+
+angular.module('lpUserService',[
+	'socket',
+	'lpConfig'
+])
+
+.factory('LpUserService',[
+'Socket','LpConfig',
+function(Socket,LpConfig){
+
+
+
+
+	return {
+		
+	};
+
+}]);
