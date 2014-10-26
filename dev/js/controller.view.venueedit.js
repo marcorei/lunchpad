72cfbe2f92ba1b0547eff9491bcd2852400f1b04
Markus Riegel <riegel.markus@googlemail.com>
2014-10-26 15:56:14 +0100

    stubs for edit view

---
+++ dev/js/controller.view.venueedit.js
@@ -0,0 +1,17 @@
+/*
+* Controller for creating new venue or editing them
+*
+* @author Markus Riegel <riegel.markus@googlemail.com>
+*/
+
+angular.module('viewVenueEditController',[
+'lpVenueService'
+])
+
+.controller('ViewVenueEditController',[
+'$scope','$location','LpVenueService',
+function($scope,$location,LpVenueService){
+
+	// Diesdas
+
+}]);
