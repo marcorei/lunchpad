e91ae32b24339f993894bc75e6f8be4033261bf1
Markus Riegel <riegel.markus@googlemail.com>
2014-10-26 18:43:07 +0100

    Impemented Crontask module

--- node/module.crontasks.js
+++ node/module.crontasks.js
@@ -5,12 +5,28 @@
  */
 
 
+var CronJob = require('cron').CronJob;
 
-var CronTasks = function(){};
+var CronTasks = function(){
+	this.tasks = [];
+};
 
+CronTasks.prototype.setupTasks = function(tasks){
+	var i;
+	for(i = 0; i < tasks.length; i++){
+		this.tasks.push(new CronJob({
+			cronTime: tasks[i].time,
+			onTick: tasks[i].fn,
+			start: true
+		}));
+	}
+}
 
-
-
+CronTasks.prototype.clearTasks = function(){
+	while(this.tasks.length > 0){
+		this.tasks.splice(i,1).stop();
+	}
+}
 
 
 exports.cronTasks = new CronTasks();
