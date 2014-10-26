edf78a9a42256278c3877354023ffec8783275bb
Markus Riegel <riegel.markus@googlemail.com>
2014-10-26 18:43:32 +0100

    added todo comments

--- node/module.mailer.js
+++ node/module.mailer.js
@@ -7,6 +7,8 @@
 
 
 var nodemailer = require('nodemailer'),
+	// require mailer-templates
+	// require templating engine
 	config = require('./config.json');
 
 
@@ -14,8 +16,10 @@ var nodemailer = require('nodemailer'),
 var Mailer = function(){};
 
 
+// TODO: consider sending via dedicated SMTP Service later on.
+// with queuing and stuff.
 
-
-
+// TODO: write wrapper for node-mailer-templates and nodemailer
+// as well as batch-sending loop
 
 exports.mailer = new Mailer();
