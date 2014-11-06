1d1719d8fdf607e226738cd64f2d3aada672d094
Markus Riegel <riegel.markus@googlemail.com>
2014-11-06 22:20:17 +0100

    basic data loading

--- dev/js/controller.view.admin.js
+++ dev/js/controller.view.admin.js
@@ -5,11 +5,23 @@
 */
 
 angular.module('viewAdminController',[
+	'lpUserService',
+	'lpItemService'
 ])
 
 .controller('ViewAdminController',[
-'$scope','LpUserService',
-function($scope,LpUserService){
+'$scope','LpUserService','LpItemService',
+function($scope,LpUserService,LpItemService){
 
-	
+	$scope.users;
+	$scope.items;
+
+	LpUserService.readUserList(function(users)){
+		$scope.users = users;
+	}
+
+	LpItemService.loadItems(function(items){
+		$scope.items = items;
+	})
+
 }]);
